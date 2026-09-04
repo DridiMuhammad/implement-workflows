cd1c5c634fce54ee2bed5dc1db3999109d999e0d
DridiMuhammad <muhammad.dridi@edu.ece.fr>
2026-09-04 17:03:59 +0200

    test

--- src/cpu_monitor.c
+++ src/cpu_monitor.c
@@ -1,7 +1,11 @@
 #include "cpu_monitor.h"
 
 #include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
 
 double cpu_monitor_get_usage_percent(void)
 {
@@ -10,7 +14,13 @@ double cpu_monitor_get_usage_percent(void)
         return -1.0;
     }
 
+#ifdef _WIN32
+    SYSTEM_INFO system_info;
+    GetSystemInfo(&system_info);
+    long num_cores = (long)system_info.dwNumberOfProcessors;
+#else
     long num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
     if (num_cores < 1) {
         num_cores = 1;
     }
